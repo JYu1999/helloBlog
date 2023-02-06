d34c601cd59676c8b3f6ccb65d1b18283bd028e9
JYu1999 <jk29666338@gmail.com>
2023-02-06 11:57:17 +0800

    feat: show all posts with their category

---
+++ resources/views/post.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/app.css">
+    <script src="/app.js"></script>
+    <title>My Blog</title>
+</head>
+<body>
+<article>
+ <h1>{!!  $post->title !!}</h1>
+    <div>
+        <p>{{$post->body}}</p>
+
+    </div>
+</article>
+
+<a href="/">Go back</a>
+
+</body>
+
+</html>
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,16 +1,16 @@
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="/app.css">
-    <script src="/app.js"></script>
-    <title>My Blog</title>
-</head>
-<body>
-    <h1>Hello world</h1>
-</body>
+@extends('layouts/layout')
 
-</html>
+@section('content')
+    @foreach($posts as $post)
+        <article>
+            <h2><a href="posts/{{$post->title}}">{{$post->title}}</a></h2>
+            <p>
+                <a href="#">{{$post->category->name}}</a>
+            </p>
+            <div>
+                {{$post->excerpt}}
+            </div>
+        </article>
+    @endforeach
+@endsection
+
