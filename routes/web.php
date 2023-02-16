b9522db33bd15ce50379b3a7139d22111e7ff23c
JYu1999 <jk29666338@gmail.com>
2023-02-16 13:36:05 +0800

    refactor: grouping AdminPostController to routers

--- routes/web.php
+++ routes/web.php
@@ -27,25 +27,25 @@ Route::post('/newsletter',\App\Http\Controllers\NewsletterController::class);
 
 
 Route::get('/', [PostController::class,'index'])->name('home');
-
 Route::get('/posts/{post:title}', [PostController::class,'show']);
+
 Route::post('posts/{post:title}/comments', [CommentController::class,'store']);
 
 
 Route::get('register',[RegisterController::class, 'create'])->middleware('guest');
 Route::post('register',[RegisterController::class,'store'])->middleware('guest');
+
 Route::get('login', [SessionsController::class,'create'])->middleware('guest');
 Route::post('login',[SessionsController::class,'store'])->middleware('guest');
-
 Route::post('logout',[SessionsController::class, 'destroy'])->middleware('auth');
 
 
-Route::middleware('can:admin')->group(function(){
-    Route::get('admin/posts/create', [AdminPostController::class, 'create']);
-    Route::post('admin/posts', [AdminPostController::class, 'store']);
-    Route::get('admin/posts', [AdminPostController::class, 'index']);
-    Route::get('admin/posts/{post}/edit',[AdminPostController::class, 'edit']);
-    Route::patch('admin/posts/{post}', [AdminPostController::class,'update']);
-    Route::delete('admin/posts/{post}', [AdminPostController::class,'destroy']);
+Route::middleware('can:admin')->controller(AdminPostController::class)->group(function(){
+    Route::get('admin/posts/create', 'create');
+    Route::post('admin/posts',  'store');
+    Route::get('admin/posts',  'index');
+    Route::get('admin/posts/{post}/edit', 'edit');
+    Route::patch('admin/posts/{post}', 'update');
+    Route::delete('admin/posts/{post}', 'destroy');
 });
 
